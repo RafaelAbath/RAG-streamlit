74a54fc4f1bb19059e5b00dc960a7b02ba9568ce
RafaelAbath <rafael.abath.274@gmail.com>
2025-03-27 19:38:22 -0300

    conclusão

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from utils import split, embbedings
 from dotenv import load_dotenv
+from streamlit_chat import message
 import os
 
-# Carregar variáveis do arquivo .env
+
 load_dotenv()
 
 
@@ -11,19 +12,37 @@ def main():
     st.set_page_config(
         page_title="RAG Com Streamlit")
     st.title("RAG Chatbot")
-    st.write("O RAG Chatbot é um chatbot que utiliza os seus documentos para responder perguntas.")
+    st.write("O RAG Chatbot é um chatbot que utiliza os seus documentos para responder perguntas.")
+    
+    st.header("Converse com o RAG Chatbot")
+    user_questions = st.text_input("Digite sua pergunta")
     
     
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+
+    if user_questions and st.session_state.conversation:
+        response = st.session_state.conversation(user_questions)['chat_history'][-1]
+        message(user_questions, is_user=True)
+        message(response.content, is_user=False)
+
+        st.write(response.content)
+
     with st.sidebar:
-        st.subheader("seus arquivos")
+        st.subheader("Seus arquivos")
         pdf_loader = st.file_uploader("Carregar PDF", type=["pdf"], accept_multiple_files=True)
        
         if st.button("Processar"):
+          
             processados = split.process_files(pdf_loader)
-           
             chunks = split.split_text_chunks(processados)
             vectorstore = embbedings.get_embeddings(chunks)
             st.write("Processado com sucesso")
+            
+          
+            st.session_state.conversation = embbedings.create_conversation(vectorstore)
+
 
 if __name__ == "__main__":
     main()
--- utils/embbedings.py
+++ utils/embbedings.py
@@ -1,7 +1,20 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
 
 def get_embeddings(chunks):
     embeddings= OpenAIEmbeddings()
     vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
     return vectorstore
+
+def create_conversation(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    conversastion_chain = ConversationalRetrievalChain.from_llm(
+       llm=llm,
+       retriever=vectorstore.as_retriever(),
+       memory=memory
+   )
+    return conversastion_chain
